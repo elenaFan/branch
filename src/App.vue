c28233dd18b60bd51e462483fb9be227e633566b
elenaFan <415109102@qq.com>
2019-04-14 22:04:36 +0800

    first

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <h1>Hello</h1>
+    <h2>hi</h2>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
